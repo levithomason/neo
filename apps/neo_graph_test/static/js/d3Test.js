e9cefb2ddc7bdffd13e06a050c0b9d6bac423553
Levi Thomason <me@levithomason.com>
2013-12-08 00:35:10 -0800

    loads countries and people onto graph, can click/drag to create new nodes

--- apps/neo_graph_test/static/js/d3Test.js
+++ apps/neo_graph_test/static/js/d3Test.js
@@ -1,26 +1,174 @@
-///////////////////////////////// new stuff //////////////////////////////////////
+/**********************************************************************************************************
+ * Utils
+ **********************************************************************************************************/
+
+// mouse down tracking
+var mouseDown;
+document.body.onmousedown = function() {
+    mouseDown = true;
+};
+document.body.onmouseup = function() {
+    mouseDown = false;
+};
+
+
+/**********************************************************************************************************
+ * Helpers
+ **********************************************************************************************************/
+
+var helpers;
+
+helpers = {
+    'random': {
+        'range': function(min, max, gutter) {
+            gutter = (typeof gutter === "undefined") ? 0 : gutter;
+
+            return Math.random() * ((max - gutter) - (min + gutter)) + min + gutter;
+        }
+    }
+};
+
+
+/**********************************************************************************************************
+ * Ready
+ **********************************************************************************************************/
 
 $(document).ready(function() {
 
-    var canvas = d3.select('#stage').append('svg');
+    /*
+    Setup d3
+     */
+    var graph = d3.select('#stage').append('svg');
+    var svg = graph[0][0];
 
+    /*
+     load all countries / citizens
+     */
+    var jsonPeople = $('.json.people').html();
+    var jsonCountries = $('.json.countries').html();
+    
+    var people = $.parseJSON(jsonPeople);
+    var countries = $.parseJSON(jsonCountries);
 
-    $('#stage').click(function(e) {
-        var offset = $(this).offset();
+    var styleCountry = {
+        'radius': 30
+    };
 
-        c = canvas.append("circle");
+    var stylePerson = {
+        'radius': 15
+    };
 
-        c.attr('r', 100);
-        c.attr('cx', e.clientX - offset.left);
-        c.attr('cy', e.clientY - offset.top);
+    /*
+    Create country / person elements
+     */
+    var circle;
+    var r;
+    var x;
+    var y;
+    
+    for (var country in countries) {
+        r = styleCountry.radius;
+
+        circle = graph.append('circle');
+
+        x = helpers.random.range(0, 1, r / svg.clientWidth) * 100;
+        y = helpers.random.range(0, 1, r / svg.clientHeight) * 100;
+
+        circle
+            .attr('class', 'node country')
+            .attr('r', r)
+            .attr('cx', x + "%")
+            .attr('cy', y + "%");
+    }
+    
+    for (var person in countries) {
+        r = stylePerson.radius;
+
+        circle = graph.append('circle');
+
+        x = helpers.random.range(0, 1, r / svg.clientWidth) * 100;
+        y = helpers.random.range(0, 1, r / svg.clientHeight) * 100;
+
+        circle
+            .attr('class', 'node person')
+            .attr('r', r)
+            .attr('cx', x + "%")
+            .attr('cy', y + "%");
+    }
+
+    /*
+     Click - add new item
+     */
+    $('#stage')
+        .mousedown(function(e) {
+            var offset = $(this).offset();
+
+            var mouseX = e.clientX - offset.left;
+            var mouseY = e.clientY - offset.top;
+
+            var circle = graph.append("circle");
+
+            circle
+                .attr('class', 'new_cirlce dragging')
+                .attr('r', 20)
+                .attr('cx', mouseX)
+                .attr('cy', mouseY)
+                .style('fill', 'transparent')
+                .style('stroke', '#749974')
+                .style('stroke-width', '4px');
+            
+            console.log(circle[0][0]);
+        })
+        .mousemove(function(e) {
+            var offset = $(this).offset();
+
+            var mouseX = e.clientX - offset.left;
+            var mouseY = e.clientY - offset.top;
+
+            if (mouseDown) {
+                d3.selectAll('.new_cirlce.dragging')
+                    .attr('cx', mouseX)
+                    .attr('cy', mouseY);
+            }
+        })
+        .mouseup(function(e) {
+            var offset = $(this).offset();
+
+            var mouseX = e.clientX - offset.left;
+            var mouseY = e.clientY - offset.top;
+
+            d3.selectAll('.new_cirlce.dragging')
+                .attr('class', 'new_circle')
+                .attr('cx', mouseX)
+                .attr('cy', mouseY)
+                .style('fill', '#AAAAAA')
+                .style('stroke', 'transparent')
+                .style('stroke-width', '0');
+        });
+
+    /*
+     force directed layout TODO: this is not working yet, check docs
+     */
+    var nodes = [];
+    var links = [];
+
+    var force = d3.layout.force()
+        .nodes(nodes)
+        .links(links)
+        .size([graph.width, graph.height])
+        .start();
+
+    force.on("tick", function() {
+        //console.log('its ticking!');
     });
+
 });
 
 
 ///////////////////////////////// old stuff for reference //////////////////////////////////////
 
 var generate_random = function() {
-    var canvas = d3.select('#stage').append('svg');
+    var g = d3.select('#stage').append('svg');
 
     var dataset = [];
 
@@ -32,7 +180,7 @@ var generate_random = function() {
     var min = d3.min(dataset);
     var loop = 0;
 
-    canvas.selectAll("circle")
+    graph.selectAll("circle")
         .data(dataset)
         .enter()
         .append("circle");
--- apps/neo_graph_test/views.py
+++ apps/neo_graph_test/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+from apps.citizens.models import Person, Country
+import json
 
 def create_graph(request):
+    # get the objects
+    people_category = Person.category()
+    people = people_category.instance.all()
 
-    return render(request, 'create_graph.html')
+    country_category = Country.category()
+    countries = country_category.instance.all()
+
+    # create dictionaries
+    people_dict = {}
+    for p in people:
+        person = {'name': p.name, 'age': p.age}
+
+        people_dict[p.name] = person
+
+    countries_dict = {}
+    for c in countries:
+        country = {'code': c.code}
+
+        countries_dict[c.code] = country
+
+    # make json objects
+    people_json = json.dumps(people_dict)
+    countries_json = json.dumps(countries_dict)
+
+    return render(request, 'create_graph.html', {'people': people_json, 'countries': countries_json})
